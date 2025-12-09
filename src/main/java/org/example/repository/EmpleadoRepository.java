6e699e0f8a0e96da446a787e52688ffa48c67de8
RaúlOcaña <rauloca06@gmail.com>
2025-12-09 09:06:26 +0100

    Actualización de empleados repositopry

--- src/main/java/org/example/repository/EmpleadoRepository.java
+++ src/main/java/org/example/repository/EmpleadoRepository.java
@@ -2,7 +2,9 @@ package org.example.repository;
 
 import org.example.model.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
 }
 
